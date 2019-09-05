e40edb1dd47e8dad4ce22f900bda717fe7006a04
wendy.hsiao <wendy19940331@gmail.com>
2019-09-06 00:06:43 +0800

    Add route

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ const Todo = require('./models/todo')
 app.get('/', (req, res) => {
   res.send('HELLo')
 })
+app.get('/todos', (req, res) => {
+  res.send('列出所有 Todo')
+})
+app.get('/todos/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+app.post('/todos', (req, res) => {
+  res.send('建立 Todo')
+})
+app.get('/todos/:id', (req, res) => {
+  res.send('顯示一筆 Todo 的詳細內容')
+})
+app.get('/todos/:id/edit', (req, res) => {
+  res.send('修改 Todo 頁面')
+})
+app.post('/todos/:id/edit', (req, res) => {
+  res.send('修改 Todo')
+})
+app.get('/todos/:id', (req, res) => {
+  res.send('顯示一筆todo')
+})
+app.post('/todos/:id/delete', (req, res) => {
+  res.send('刪除 Todo')
+})
 
 app.listen(3000, () => {
   console.log('done')
