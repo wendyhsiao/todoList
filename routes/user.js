1bed6ce4b8c8dec9665bf83c9d5692dee5e880b9
wendy.hsiao <wendy19940331@gmail.com>
2019-09-10 00:14:26 +0800

    Add register page

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ router.post('/login', (req, res) => {
 })
 
 router.get('/register', (req, res) => {
-  res.send('register')
+  res.render('register')
 })
 
 router.post('/register', (req, res) => {
