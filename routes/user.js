195d699c305ba1bcd614bbe4b066a3380408319c
wendy.hsiao <wendy19940331@gmail.com>
2019-09-09 23:49:22 +0800

    Add login page

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
-  res.send('login')
+  res.render('login')
 })
 
 router.post('/login', (req, res) => {
